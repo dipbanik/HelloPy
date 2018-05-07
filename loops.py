26edb763714484602069fdf2c6ccbe01ebe6ac99
Dip Banik <dip.banik@live.com>
2018-05-08 02:35:49 +0530

    adding loops
    Signed-off-by: Dip Banik <dip.banik@live.com>

---
+++ loops.py
@@ -0,0 +1,8 @@
+student_names = ['James', 'Katrina', 'Jessica']
+
+for student in student_names :
+    if student :
+        if student == 'James' :
+            print('Found him!')
+            break;
+    print('exit loop')
